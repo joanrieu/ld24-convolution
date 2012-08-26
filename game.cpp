402230f3c7cd740c9baa192eb00797a6b9c53bd2
Joan Rieu <toto_pirate@hotmail.fr>
2012-08-26 02:51:39 +0200

    Created a Game of Life with teams and lives

---
+++ game.cpp
@@ -0,0 +1,183 @@
+#include <SFML/Graphics.hpp>
+
+struct Team {
+        std::string name;
+        sf::Color color;
+};
+
+struct Cell {
+        Team* team;
+        int lives;
+};
+
+enum class Stuff {
+        DeadCell
+};
+
+struct sfVector2i_less {
+        bool operator()(const sf::Vector2i& v1, const sf::Vector2i& v2) {
+                return v1.y < v2.y or (v1.y == v2.y and v1.x < v2.x);
+        }
+};
+
+struct Grid {
+        std::map<sf::Vector2i, Cell, sfVector2i_less> cells;
+        std::map<sf::Vector2i, Stuff, sfVector2i_less> stuff;
+        void update();
+};
+
+void Grid::update() {
+
+        std::map<sf::Vector2i, std::vector<Team*>, sfVector2i_less> adjacency;
+
+        std::for_each(cells.begin(), cells.end(), [&](const std::pair<sf::Vector2i, Cell>& cellInfo) {
+
+                auto& cellPos(cellInfo.first);
+                auto& cell(cellInfo.second);
+
+                sf::Vector2i neighbourPos;
+                for (neighbourPos.y = cellPos.y - 1; neighbourPos.y <= cellPos.y + 1; ++neighbourPos.y)
+                        for (neighbourPos.x = cellPos.x - 1; neighbourPos.x <= cellPos.x + 1; ++neighbourPos.x)
+                                adjacency[neighbourPos].push_back(cell.team);
+
+        });
+
+        decltype(cells) nextCells;
+
+        std::for_each(adjacency.begin(), adjacency.end(), [&](const std::pair<sf::Vector2i, std::vector<Team*>>& adjacencyInfo) {
+
+                auto& cellPos(adjacencyInfo.first);
+                auto& neighbours(adjacencyInfo.second);
+                int neighbourCount = neighbours.size();
+
+                Team* winningTeam;
+
+                {
+
+                        std::map<Team*, int> teamMembers;
+                        std::for_each(neighbours.begin(), neighbours.end(), [&](Team* team) {
+                                ++teamMembers[team];
+                        });
+
+                        std::map<int, Team*> sortedTeams;
+                        std::for_each(teamMembers.begin(), teamMembers.end(), [&](std::pair<Team*, int> teamInfo) {
+                                sortedTeams.insert(std::make_pair(teamInfo.second, teamInfo.first));
+                        });
+
+                        winningTeam = sortedTeams.rbegin()->second;
+
+                }
+
+                auto cellIt = cells.find(cellPos);
+
+                if (cellIt != cells.end()) {
+
+                        --neighbourCount; // The tile is counted as its own neighbour for team counting purposes.
+
+                        Cell cell = cellIt->second;
+                        cell.team = winningTeam;
+
+                        if (neighbourCount == 2 or neighbourCount == 3 or --cell.lives > 0)
+                                nextCells.insert(std::make_pair(cellPos, cell));
+
+                } else if (neighbourCount == 3) { // Cells can only appear with three neighbours
+
+                        Cell cell;
+                        cell.team = winningTeam;
+                        cell.lives = 1;
+
+                        auto stuffIt = stuff.find(cellPos);
+
+                        if (stuffIt != stuff.end()) {
+
+                                if (stuffIt->second == Stuff::DeadCell)
+                                        ++cell.lives;
+
+                                stuff.erase(stuffIt);
+
+                        }
+
+                        nextCells.insert(std::make_pair(cellPos, cell));
+
+                }
+
+        });
+
+        cells.swap(nextCells);
+
+}
+
+int main() {
+
+        Team playerTeam = { "Player", sf::Color(200, 50, 50) };
+        Team enemy1Team = { "Enemy 1", sf::Color(50, 200, 50) };
+        Team enemy2Team = { "Enemy 2", sf::Color(50, 50, 200) };
+        Cell playerCell = { &playerTeam, 1 };
+        Cell enemy1Cell = { &enemy1Team, 1 };
+        Cell enemy2Cell = { &enemy2Team, 1 };
+
+        Grid grid;
+        grid.cells.insert(std::make_pair(sf::Vector2i(0, 0), playerCell));
+        grid.cells.insert(std::make_pair(sf::Vector2i(1, 0), playerCell));
+        grid.cells.insert(std::make_pair(sf::Vector2i(2, 0), playerCell));/*
+        grid.cells.insert(std::make_pair(sf::Vector2i(2, -1), playerCell));
+        grid.cells.insert(std::make_pair(sf::Vector2i(1, -2), playerCell));
+        grid.cells.insert(std::make_pair(sf::Vector2i(0, 1), enemy1Cell));
+        grid.cells.insert(std::make_pair(sf::Vector2i(1, 1), enemy1Cell));*/
+        grid.cells.insert(std::make_pair(sf::Vector2i(2, 1), enemy1Cell));/*
+        grid.cells.insert(std::make_pair(sf::Vector2i(0, 2), enemy2Cell));*/
+        grid.cells.insert(std::make_pair(sf::Vector2i(1, 2), enemy2Cell));
+        grid.cells.insert(std::make_pair(sf::Vector2i(2, 2), enemy2Cell));
+
+        sf::RenderWindow win(sf::VideoMode(800, 600), "Convolution");
+        win.setVerticalSyncEnabled(true);
+        win.setKeyRepeatEnabled(false);
+
+        const float scaleStep = 1.5f;
+        float scaling = 100.f;
+        sf::Vector2f gridAim;
+
+        while (win.isOpen()) {
+
+                const float moveStep = .05f * win.getSize().x / scaling;
+
+                sf::Event event;
+                while (win.pollEvent(event)) {
+
+                        if (event.type == sf::Event::Closed)
+                                win.close();
+
+#                       define BIND(keyname, action) else if (event.type == sf::Event::KeyPressed and event.key.code == sf::Keyboard::keyname) action
+                        BIND(Escape, win.close());
+                        BIND(Space, grid.update());
+                        BIND(Add, scaling *= scaleStep);
+                        BIND(Subtract, scaling /= scaleStep);
+                        BIND(Up, gridAim.y -= moveStep);
+                        BIND(Down, gridAim.y += moveStep);
+                        BIND(Left, gridAim.x -= moveStep);
+                        BIND(Right, gridAim.x += moveStep);
+#                       undef BIND
+
+                }
+
+                win.clear();
+
+                std::for_each(grid.cells.begin(), grid.cells.end(), [&](const std::pair<sf::Vector2i, Cell> pair) {
+
+                        sf::Vector2f cellPos(pair.first.x, pair.first.y);
+                        cellPos -= gridAim;
+                        cellPos *= scaling;
+                        cellPos += .5f * sf::Vector2f(win.getSize().x, win.getSize().y);
+
+                        sf::RectangleShape cellShape(sf::Vector2f(scaling, scaling));
+                        cellShape.setPosition(cellPos);
+                        cellShape.setFillColor(pair.second.team->color);
+                        win.draw(cellShape);
+
+                });
+
+                win.display();
+
+        }
+
+}
---
+++ premake4.lua
@@ -0,0 +1,11 @@
+solution "ld24"
+        configurations { "Debug", "Release" }
+        configuration "Debug"
+                flags "Symbols"
+        configuration "Release"
+                flags "Optimize"
+        project "convolution"
+                kind "WindowedApp"
+                language "C++"
+                files "**.cpp"
+                links { "sfml-system", "sfml-window", "sfml-graphics" }
